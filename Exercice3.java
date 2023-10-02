cb227a325acd0c78378b16df1774352158126a02
Rayane Besrour <91121169+Rayane-94@users.noreply.github.com>
2023-10-02 10:27:51 +0200

    Add files via upload

---
+++ Exercice3.java
@@ -0,0 +1,10 @@
+public class Exercice3 {
+    public static void main(String[] args) {
+        String name = "Hall";
+        Integer age = 27;
+        String job = "développeur";
+
+        System.out.println(" Je m'appelle " + name + ", j'ai " + age + " et je suis " + job);
+
+    }
+}
