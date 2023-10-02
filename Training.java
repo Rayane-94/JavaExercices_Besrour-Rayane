06c089c3266b0d3d72060609f7dab98f577751d1
Rayane Besrour <91121169+Rayane-94@users.noreply.github.com>
2023-10-02 10:04:41 +0200

    Add files via upload

---
+++ Training.java
@@ -0,0 +1,9 @@
+public class Training {
+    public static void main(String[] args) {
+
+        String name = "Alex";
+        Integer age = 25;
+        String concatenation = name + age;
+        System.out.println(concatenation);
+    }
+}
